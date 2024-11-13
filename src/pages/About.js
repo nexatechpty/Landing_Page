5cdfaa64e95af45dc5add13a6c2f02555143beef
Prosper Sibanda <nexatech2025@gmail.com>
2024-11-13 16:28:56 +0200

    merge

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,29 +1,32 @@
-import React, {Fragment} from 'react';
-import {Seo, Breadcrumb, ScrollTopBtn} from '../components';
+import React, { Fragment } from "react";
+import { Seo, Breadcrumb, ScrollTopBtn } from "../components";
 import {
-  HeaderOne,
-  ServiceFour, 
+  ServiceFour,
   AboutSeven,
   TeamTwo,
   TestimonialThree,
   CtaOne,
   BlogFour,
-  FooterOne
-} from '../containers';
+  FooterOne,
+  HeaderFour,
+} from "../containers";
 
-const About = ()=> (
+const About = () => (
   <Fragment>
     <Seo title="About Us" />
-    <HeaderOne />
-    <Breadcrumb pageName="About Us" bgImage="images/breadcumb/breadcumb-bg.jpg" />
-    <ServiceFour className="space-top space-extra-bottom"/>
-    <AboutSeven/>
-    <TeamTwo className="space-top space-extra-bottom"/>
-    <TestimonialThree className="space-top space-extra-bottom"/>
-    <CtaOne className="space"/>
-    <BlogFour className="space-top space-extra-bottom"/>
-    <FooterOne/>
-    <ScrollTopBtn/>
+    <HeaderFour />
+    <Breadcrumb
+      pageName="About Us"
+      bgImage="images/breadcumb/breadcumb-bg.jpg"
+    />
+    <ServiceFour className="space-top space-extra-bottom" />
+    <AboutSeven />
+    <TeamTwo className="space-top space-extra-bottom" />
+    <TestimonialThree className="space-top space-extra-bottom" />
+    <CtaOne className="space" />
+    <BlogFour className="space-top space-extra-bottom" />
+    <FooterOne />
+    <ScrollTopBtn />
   </Fragment>
 );
 
--- src/pages/Service.js
+++ src/pages/Service.js
@@ -1,25 +1,28 @@
-import React, {Fragment} from 'react';
-import {Seo, Breadcrumb, ScrollTopBtn} from '../components';
+import React, { Fragment } from "react";
+import { Seo, Breadcrumb, ScrollTopBtn } from "../components";
 import {
-  HeaderOne,
   ServiceFive,
   CtaOne,
   ProcessThree,
   TestimonialOne,
-  FooterOne
-} from '../containers';
+  FooterOne,
+  HeaderFour,
+} from "../containers";
 
-const Service = ()=> (
+const Service = () => (
   <Fragment>
     <Seo title="Services" />
-    <HeaderOne />
-    <Breadcrumb pageName="Services" bgImage="images/breadcumb/breadcumb-bg.jpg" />
-    <ServiceFive className="space-top space-extra-bottom"/>
-    <CtaOne className="space"/>
-    <ProcessThree className="space-top space-extra-bottom"/>
-    <TestimonialOne className="space-top space-extra-bottom"/>
-    <FooterOne/>
-    <ScrollTopBtn/>
+    <HeaderFour />
+    <Breadcrumb
+      pageName="Services"
+      bgImage="images/breadcumb/breadcumb-bg.jpg"
+    />
+    <ServiceFive className="space-top space-extra-bottom" />
+    <CtaOne className="space" />
+    <ProcessThree className="space-top space-extra-bottom" />
+    <TestimonialOne className="space-top space-extra-bottom" />
+    <FooterOne />
+    <ScrollTopBtn />
   </Fragment>
 );
 
